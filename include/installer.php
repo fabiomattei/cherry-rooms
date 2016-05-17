445dd4a869092640782f4cb2d6f6a786d763eea9
Fabio Mattei <burattino@gmail.com>
2016-05-17 19:03:52 +0100

    adding default settings

--- include/installer.php
+++ include/installer.php
@@ -1,3 +1,21 @@
 <?php
+
+/**
+ * Create the necessary default settings 
+ */
+function rcrm_items_create_settings() {
 	
-// empty for now
+    $DefaultSettingsArray = serialize( array(
+		"RCSL_Slide_Title"   		=> 1,
+		"RCSL_Auto_Slideshow"   	=> 1,
+		"RCSL_Sliding_Arrow"   		=> 1,
+		"RCSL_Slider_Navigation"   	=> 1,
+		"RCSL_Navigation_Button"   	=> 1,
+		"RCSL_Slider_Width"   		=> 1000,
+		"RCSL_Slider_Height"   		=> 500
+    ));
+    add_option("RCSL_Settings", $DefaultSettingsArray);
+	
+}
+
+register_activation_hook( __FILE__, 'rcrm_items_create_settings' );
