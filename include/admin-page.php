2c271f204aed7d34a5f3902e5344591beb9f8839
Fabio Mattei <burattino@gmail.com>
2016-05-18 16:57:03 +0100

    inserting color picker

--- include/admin-page.php
+++ include/admin-page.php
@@ -1,158 +1,224 @@
 <?php
+ 
+/**
+ * Option panel class
+ */
+class RCRM_Options {
+  
+    /*--------------------------------------------*
+     * Attributes
+     *--------------------------------------------*/
+  
+    /** Refers to a single instance of this class. */
+    private static $instance = null;
+     
+    /* Saved options */
+    public $options;
+  
+    /*--------------------------------------------*
+     * Constructor
+     *--------------------------------------------*/
+  
+    /**
+     * Creates or returns an instance of this class.
+     *
+     * @return  CPA_Theme_Options A single instance of this class.
+     */
+    public static function get_instance() {
+  
+        if ( null == self::$instance ) {
+            self::$instance = new self;
+        }
+  
+        return self::$instance;
+  
+    } // end get_instance;
+  
+    /**
+     * Initializes the plugin by setting localization, filters, and administration functions.
+     */
+	private function __construct() { 
+	    // Add the page to the admin menu
+	    add_action( 'admin_menu', array( &$this, 'add_page' ) );
+     
+	    // Register page options
+	    add_action( 'admin_init', array( &$this, 'register_page_options') );
+     
+	    // Css rules for Color Picker
+	    wp_enqueue_style( 'wp-color-picker' );
+     
+	    // Register javascript
+	    add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_js' ) );
+     
+	    // Get registered option
+	    $this->options = get_option( RCRO_SETTINGS_KEY );
+	}
+  
+    /*--------------------------------------------*
+     * Functions
+     *--------------------------------------------*/
+      
+	/**
+	 * Function that will add the options page under Setting Menu.
+	 */
+	public function add_page() { 
+	    add_options_page(
+	        __('Cherry Rooms', 'cherryroomssettings'),      // page_title
+	        __('Cherry Rooms', 'cherryroomssettings'),      // menu_title
+	        'manage_options',                               // access level required to see the page
+	        'rc-rm-cherry-rooms',                           // menu slug
+	        array( $this, 'display_page' )                  // callback function
+	    );
+	}
+ 
+	/**
+	 * Function that will display the options page.
+	 */
+	public function display_page() { 
+	    ?>
+	    <div class="wrap">
+	        <h2>Cherry Rooms Settings</h2>
+	        <form action="options.php" method="post">
+	            <?php
+	                settings_fields( 'rcrmoptiongroup' );            // refers to option group
+	                do_settings_sections( 'rc-rm-cherry-rooms' );    // refers to page slug or id
+	                submit_button();
+	            ?>
+	        </form>
+	    </div>
+		<?php  
+	}
+       
+	/**
+	 * Function that will register admin page options.
+	 */
+	public function register_page_options() { 
+	    
+	    add_settings_section(
+	        'rc_rm_cherry_rooms_section',                     // id
+	        __('Settings', 'cherryroomsettings'),             // Section title
+	        array( $this, 'display_section' ),                // callback function
+	        'rc-rm-cherry-rooms'                              // page id
+	    );
 
-// Add the admin options page
-add_action('admin_menu', 'rc_rm_cherry_rooms_add_page');
+	    add_settings_field(
+	        'rc_rm_cherry_rooms_setting_input',             // id
+	        __('Speed', 'cherryroomsettings'),              // lable to show in the form associated to the field
+	        array( $this, 'speed_settings_field' ),         // callback function
+	        'rc-rm-cherry-rooms',                           // page id
+	        'rc_rm_cherry_rooms_section'                    // section id
+	    );
 
-function rc_rm_cherry_rooms_add_page() {
-    add_options_page(
-        __('Cherry Rooms', 'cherryroomssettings'),
-        __('Cherry Rooms', 'cherryroomssettings'),
-        'manage_options',                               // access level required to see the page
-        'rc-rm-cherry-rooms',                          // menu slug
-        'rc_rm_cherry_rooms_options_page_callback'     // callback function
-    );
-}
+	    add_settings_field(
+	        'rc_rm_cherry_rooms_transition',                // id
+	        __('Transition', 'cherryroomsettings'),         // lable to show in the form associated to the field
+	        array( $this, 'transition_settings_field' ),    // callback function
+	        'rc-rm-cherry-rooms',                           // page id
+	        'rc_rm_cherry_rooms_section'                    // section id
+	    );
+		
+	    // Add Background Color Field
+	    add_settings_field( 
+			'cpa_bg_field',                                 // id
+			__('Background Color', 'cherryroomsettings'),   // lable to show in the form associated to the field
+			array( $this, 'bg_settings_field' ),            // callback function
+			'rc-rm-cherry-rooms',                           // page id
+			'rc_rm_cherry_rooms_section'                    // section id
+		);
+		
+	    // activating settings
+	    register_setting(
+	        'rcrmoptiongroup',                                // option group
+	        'rc_rm_options',                                  // option name, determine the name of the setting stored in the database
+	        array( $this, 'validate_options' )                // callback function for validation
+	    );
+	}
+     
+	/**
+	 * Function that will add javascript file for Color Piker.
+	 */
+	public function enqueue_admin_js() { 
+	    // Make sure to add the wp-color-picker dependecy to js file
+	    wp_enqueue_script( 'cpa_custom_js', RCRO_PLUGIN_URL . 'js/jquery.custom.js', array( 'jquery', 'wp-color-picker' ), '', true  );
+	}
+     
+	/**
+	 * Function that will validate all fields.
+	 */
+	public function validate_options( $fields ) { 
+	    $valid_fields = array();
+		
+	    $valid_fields['speed'] = preg_replace( '/[^0-9]/', '', $fields['speed'] );
+	    $valid_fields['transition'] = preg_replace( '/[^a-zA-Z]/', '', $fields['transition'] );
+     
+	    // Validate Title Field
+	    $title = trim( $fields['title'] );
+	    $valid_fields['title'] = strip_tags( stripslashes( $title ) );
+     
+	    // Validate Background Color
+	    $background = trim( $fields['background'] );
+	    $background = strip_tags( stripslashes( $background ) );
+     
+	    // Check if is a valid hex color
+	    if( FALSE === $this->check_color( $background ) ) {
+     
+	        // Set the error message
+	        add_settings_error( 'cpa_settings_options', 'cpa_bg_error', 'Insert a valid color for Background', 'error' ); // $setting, $code, $message, $type
+         
+	        // Get the previous valid value
+	        $valid_fields['background'] = $this->options['background'];
+     
+	    } else {
+     
+	        $valid_fields['background'] = $background;  
+     
+	    }
+     
+	    return apply_filters( 'validate_options', $valid_fields, $fields);
+	}
+ 
+	/**
+	 * Function that will check if value is a valid HEX color.
+	 */
+	public function check_color( $value ) { 
+	    if ( preg_match( '/^#[a-f0-9]{6}$/i', $value ) ) { // if user insert a HEX color with #     
+	        return true;
+	    }
+     
+	    return false;
+	}
+     
+    /**
+     * Callback function for settings section
+     */
+    public function display_section() { /* Leave blank */ } 
+	// echo '<p>Enter your can tune the Cherry Rooms plug-in.</p>';
 
-// Draw the options page
-function rc_rm_cherry_rooms_options_page_callback() {
-    ?>
-    <div class="wrap">
-        <h2>Cherry Rooms Settings</h2>
-        <form action="options.php" method="post">
-            <?php
-                settings_fields('rcrmoptiongroup');            // refers to option group
-                do_settings_sections('rc-rm-cherry-rooms');    // refers to page slug or id
-                submit_button();
-            ?>
-        </form>
-    </div>
-<?php
-}
-
-
-function rc_rm_init() {
-    // activating settings
-    register_setting(
-        'rcrmoptiongroup',                                // option group
-        'rc_rm_options',                                  // option name, determine the name of the setting stored in the database
-        'rc_rm_cherry_rooms_validate_options'             // callback function for validation
-    );
-
-    add_settings_section(
-        'rc_rm_cherry_rooms_section',                     // id
-        __('Settings', 'cherryroomsettings'),            // Section title
-        'rc_rm_cherry_rooms_section_callback',            // callback function
-        'rc-rm-cherry-rooms'                              // page id
-    );
-
-    add_settings_field(
-        'rc_rm_cherry_rooms_setting_input',             // id
-        __('Speed', 'cherryroomsettings'),             // lable to show in the form associated to the field
-        'rc_rm_cherry_rooms_setting_input_callback',    // callback function
-        'rc-rm-cherry-rooms',                           // page id
-        'rc_rm_cherry_rooms_section'                    // section id
-    );
-
-    add_settings_field(
-        'rc_rm_cherry_rooms_transition',                // id
-        __('Transition', 'cherryroomsettings'),        // lable to show in the form associated to the field
-        'rc_rm_cherry_rooms_transition_callback',       // callback function
-        'rc-rm-cherry-rooms',                           // page id
-        'rc_rm_cherry_rooms_section'                    // section id
-    );
-
-    add_settings_field(
-        'rc_rm_cherry_rooms_easing',                    // id
-        __('Easing', 'cherryroomsettings'),            // lable to show in the form associated to the field
-        'rc_rm_cherry_rooms_easing_callback',           // callback function
-        'rc-rm-cherry-rooms',                           // page id
-        'rc_rm_cherry_rooms_section'                    // section id
-    );
+	/**
+	 * Functions that display the fields.
+	 */
+	public function speed_settings_field() { 
+	    $val = ( isset( $this->options['speed'] ) ) ? $this->options['speed'] : '';
+	    echo '<input type="text" name="rc_rm_options[speed]" value="' . $val . '" />';
+	}
 	
-    add_settings_field(
-        'rc_rm_cherry_rooms_width',                     // id
-        __('Width', 'cherryroomsettings'),            // lable to show in the form associated to the field
-        'rc_rm_cherry_rooms_width_callback',           // callback function
-        'rc-rm-cherry-rooms',                           // page id
-        'rc_rm_cherry_rooms_section'                    // section id
-    );
-	
-    add_settings_field(
-        'rc_rm_cherry_rooms_height',                    // id
-        __('Height', 'cherryroomsettings'),            // lable to show in the form associated to the field
-        'rc_rm_cherry_rooms_height_callback',           // callback function
-        'rc-rm-cherry-rooms',                           // page id
-        'rc_rm_cherry_rooms_section'                    // section id
-    );
-
-}
-
-add_action('admin_init', 'rc_rm_init');
-
-// Explanations about this section
-function rc_rm_cherry_rooms_section_callback() {
-    echo '<p>Enter your can tune the Cherry Rooms plug-in.</p>';
-}
-
-// Display and fill the form field
-function rc_rm_cherry_rooms_setting_input_callback() {
-    //get option 'text_string' value from the database
-    $options = get_option( RCRO_SETTINGS_KEY );
-    $speed = $options['speed'];
-    // echo the field
-    echo "<input id='speed' name='rc_rm_options[speed]' type='text' value='{$speed}' />";
-}
-
-function rc_rm_cherry_rooms_transition_callback() {
-    $options = get_option( RCRO_SETTINGS_KEY );
-    if( !isset( $options['transition'] ) ) $options['transition'] = 'fade';
-    ?>
-    <select name="rc_rm_options[transition]">
-        <option value="fade" <?php selected( $options['transition'], 'fade' ); ?>>fade</option>
-        <option value="horizontal" <?php selected( $options['transition'], 'horizontal' ); ?>>horizontal</option>
-        <option value="vertical" <?php selected( $options['transition'], 'vertical' ); ?>>vertical</option>
-        <option value="kenburns" <?php selected( $options['transition'], 'kenburns' ); ?>>kenburns</option>
-    </select>
-    <?php
-}
-
-function rc_rm_cherry_rooms_easing_callback() {
-    $options = get_option( RCRO_SETTINGS_KEY );
-    if( !isset( $options['easing'] ) ) $options['easing'] = 'swing';
-    ?>
-    <select name="rc_rm_options[easing]">
-        <option value="swing" <?php selected( $options['easing'], 'swing' ); ?>>swing</option>
-        <option value="linear" <?php selected( $options['easing'], 'linear' ); ?>>linear</option>
-        <option value="easeInQuad" <?php selected( $options['easing'], 'easeInQuad' ); ?>>easeInQuad</option>
-    </select>
-    <?php
-}
-
-// Display and fill the form field
-function rc_rm_cherry_rooms_width_callback() {
-    //get option 'text_string' value from the database
-    $options = get_option( RCRO_SETTINGS_KEY );
-    $width = $options['width'];
-    // echo the field
-    echo "<input id='width' name='rc_rm_options[width]' type='number' value='{$width}' />";
-}
-
-// Display and fill the form field
-function rc_rm_cherry_rooms_height_callback() {
-    //get option 'text_string' value from the database
-    $options = get_option( RCRO_SETTINGS_KEY );
-    $height = $options['height'];
-    // echo the field
-    echo "<input id='height' name='rc_rm_options[height]' type='number' value='{$height}' />";
-}
-
-// Validate input
-function rc_rm_cherry_rooms_validate_options( $input ) {
-    $valid = array();
-    $valid['speed'] = preg_replace( '/[^0-9]/', '', $input['speed'] );
-    $valid['transition'] = preg_replace( '/[^a-zA-Z]/', '', $input['transition'] );
-    $valid['easing'] = preg_replace( '/[^a-zA-Z]/', '', $input['easing'] );
-    $valid['width'] = preg_replace( '/[^0-9]/', '', $input['width'] );
-    $valid['height'] = preg_replace( '/[^0-9]/', '', $input['height'] );
-    return $valid;
-}
+	public function transition_settings_field() { 
+	    $val = ( isset( $this->options['transition'] ) ) ? $this->options['transition'] : 'fade';
+	    ?>
+	    <select name="rc_rm_options[transition]">
+	        <option value="fade" <?php selected( $val, 'fade' ); ?>>fade</option>
+	        <option value="horizontal" <?php selected( $val, 'horizontal' ); ?>>horizontal</option>
+	        <option value="vertical" <?php selected( $val, 'vertical' ); ?>>vertical</option>
+	        <option value="kenburns" <?php selected( $val, 'kenburns' ); ?>>kenburns</option>
+	    </select>
+	    <?php
+	}
+ 
+	public function bg_settings_field() { 
+	    $val = ( isset( $this->options['title'] ) ) ? $this->options['background'] : '';
+	    echo '<input type="text" name="rc_rm_options[background]" value="' . $val . '" class="rc-cr-color-picker" >';
+	}
+         
+} // end class
+  
+RCRM_Options::get_instance();
---
+++ js/jquery.custom.js
@@ -0,0 +1,6 @@
+(function( $ ) {
+    $(function() {
+        // Add Color Picker to all inputs that have 'color-field' class
+        $( '.rc-cr-color-picker' ).wpColorPicker();
+    });
+})( jQuery );
