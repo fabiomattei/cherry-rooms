e827710bab67303a30b0b3afd5e104fda019afc6
Fabio Mattei <burattino@gmail.com>
2016-05-18 17:13:16 +0100

    fixing stuff

--- include/display-functions.php
+++ include/display-functions.php
@@ -9,18 +9,16 @@ function RCSL_Cherry_Room_ShortCode_Detect() {
     $Posts = $wp_query->posts;
     $Pattern = get_shortcode_regex();
     foreach ($Posts as $Post) {
-		if ( strpos($Post->post_content, 'RCRoomListHome' ) ) {
+		if ( strpos($Post->post_content, 'RCRoomHorizontalList' ) ) {
 			// loading css scripts
-			wp_enqueue_style('rcro-css', RCRO_PLUGIN_URL.'css/rcbackbone.css');
+			wp_enqueue_style('rc-backbone-css', RCRO_PLUGIN_URL.'css/rcbackbone.css');
         } //end of if
 		
-		if ( strpos($Post->post_content, 'RCRoomListHome' ) ) {
+		if ( strpos($Post->post_content, 'RCRoomForm' ) ) {
 			// loading js scripts
 			wp_enqueue_style('rcro-jquerycss', RCRO_PLUGIN_URL.'lib/jquery-ui/jquery-ui.css');
 			wp_enqueue_script('rcro-jqueryui-javascript', RCRO_PLUGIN_URL.'lib/jquery-ui/jquery-ui.min.js', array('jquery'), '', true);
 			wp_enqueue_script('rcro-datepicker-javascript', RCRO_PLUGIN_URL.'js/rooms.js', '', '', true);
-			
-			
         } //end of if
 		
     } //end of foreach
